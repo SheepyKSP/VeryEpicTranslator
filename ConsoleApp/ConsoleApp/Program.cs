b807d1f979561eb536990ff099c101dbb6a1d2c5
SheepyKSP <74254444+SheepyKSP@users.noreply.github.com>
2021-10-29 20:02:26 -0500

    Uploaded First version
    Current words:
    hello
    cool
    i am
    are
    sheep
    is
    play
    epic
    --More words will be added soon

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Sample
+{
+	class Test
+	{
+		public static void Main(string[] args)
+		{
+			for (int i = 1; i < 10; i++)
+			{
+				string testString;
+				string lang;
+				Console.WriteLine("WELCOME TO THE VERY EPIC TRANSLATOR (Current languages: German)");
+				Console.WriteLine("Enter a word: ");
+				testString = Console.ReadLine();
+				Console.WriteLine("Enter the language you want this word to be translated to (There is currently only German): ");
+				lang = Console.ReadLine();
+				if (lang == "german")
+				{
+					if (testString == "hello")
+					{
+						Console.WriteLine("Hallo");
+					}
+					if (testString == "cool")
+					{
+						Console.WriteLine("Kühl");
+					}
+					if (testString == "i am")
+					{
+						Console.WriteLine("Ich bin");
+					}
+					if (testString == "am")
+					{
+						Console.WriteLine("Bist");
+					}
+					if (testString == "sheep")
+					{
+						Console.WriteLine("Schafe");
+					}
+					if (testString == "is")
+					{
+						Console.WriteLine("Ist");
+					}
+					if (testString == "play")
+					{
+						Console.WriteLine("Spiel");
+					}
+					if (testString == "epic")
+					{
+						Console.WriteLine("Episch");
+					}
+				}
+			}
+		}
+	}
+}
+
